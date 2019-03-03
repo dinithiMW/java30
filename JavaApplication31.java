7e6ede84120544f26ca86756b35731c84ff8294d
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-03-03 18:51:03 +0530

    java30
    java30

---
+++ JavaApplication31.java
@@ -0,0 +1,17 @@
+
+public class JavaApplication31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String myString="Hello Youtube";
+        int myStringlength=myString.length();
+        String myStringInCase=myString.toLowerCase();
+        
+        System.out.println(myString);
+        System.out.println(myStringlength);
+        System.out.println(myStringInCase);
+    }
+    
+}
